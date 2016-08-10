9334c8eda2fe2d771220a144e3f122b9bf8e7957
Daniel Castro García <dandev237@gmail.com>
2016-08-10 02:28:28 +0200

    Repository cleanup part II

---
+++ src/smartsniff-api/AsocSessionDevice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace smartsniff_api
+{
+    public partial class AsocSessionDevice
+    {
+        public int IdSession { get; set; }
+        public int IdDevice { get; set; }
+        public int IdLocation { get; set; }
+
+        public virtual Device IdDeviceNavigation { get; set; }
+        public virtual Location IdLocationNavigation { get; set; }
+        public virtual Session IdSessionNavigation { get; set; }
+    }
+}
